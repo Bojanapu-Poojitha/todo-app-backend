5652c846b6bf77a85f62b3ca94c6d20e47cbd4e3
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2025-11-18 15:19:30 +0530

    Add a route for deleting item also call the delete controller function

--- src/routes/Routers.ts
+++ src/routes/Routers.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addTask, getTask } from "../controllers/TaskOperations";
+import { addTask, getTask,deleteItemTask } from "../controllers/TaskOperations";
 
 const route = Router();
 
 route.post("/newItem", addTask);
 route.get('/tasks',getTask)
+route.delete('/item/:id',deleteItemTask);
 export default route;
