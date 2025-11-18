dfae8651a0d026bf5451749f39c106e942d3ce24
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2025-11-18 15:56:33 +0530

    Call the update service in this controller file

--- src/controllers/TaskOperations.ts
+++ src/controllers/TaskOperations.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { addTaskItem, getAllTasks, deleteTask } from "../services/TaskService";
+import {
+  addTaskItem,
+  getAllTasks,
+  deleteTask,
+  updateTask,
+} from "../services/TaskService";
 import { ItemType } from "../type/ItemType";
 
 export const addTask = async (req: Request, res: Response) => {
@@ -20,13 +25,22 @@ export const getTask = async (req: Request, res: Response) => {
     res.status(500).json({ message: "failed to add item" });
   }
 };
-export const deleteItemTask = async(req:Request,res:Response) =>{
-    try{
-        const id= req.params.id
-        await deleteTask(id);
-        res.status(400).json({message:"item deleted success fully"})
-    }
-    catch(e){
-        res.status(500).json({message:'item not deleted success fully'});
-    }
-}
+export const deleteItemTask = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    await deleteTask(id);
+    res.status(400).json({ message: "item deleted success fully" });
+  } catch (e) {
+    res.status(500).json({ message: "item not deleted success fully" });
+  }
+};
+export const updateItemTask = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const update = await updateTask(id, data);
+    res.status(200).json({ message: "item updated successfully", item: data });
+  } catch (e) {
+    res.status(500).json({ message: "item is not updated " });
+  }
+};
