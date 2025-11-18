b6f43c618582df6609af5592326d35de3513dd27
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2025-11-18 14:40:35 +0530

    Call to get task services in controllers file

--- src/controllers/TaskOperations.ts
+++ src/controllers/TaskOperations.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { addTaskItem } from "../services/TaskService";
+import { addTaskItem, getAllTasks } from "../services/TaskService";
 import { ItemType } from "../type/ItemType";
 
 export const addTask = async (req: Request, res: Response) => {
@@ -8,6 +8,14 @@ export const addTask = async (req: Request, res: Response) => {
     const storeNewItem = await addTaskItem(newTask);
 
     res.status(201).json({ message: "item created", data: storeNewItem });
+  } catch (e) {
+    res.status(500).json({ message: "failed to add item" });
+  }
+};
+export const getTask = async (req: Request, res: Response) => {
+  try {
+    const getItems = await getAllTasks();
+    res.status(200).json({ message: "the list of items are", data: getItems });
   } catch (e) {
     res.status(500).json({ message: "failed to add item" });
   }
