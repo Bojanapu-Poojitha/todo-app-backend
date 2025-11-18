60c2b41460a6bfd0a2cf3a02a87aaf4a530851fe
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2025-11-18 15:57:42 +0530

    Place the update function and create a route for that controller

--- src/routes/Routers.ts
+++ src/routes/Routers.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { addTask, getTask,deleteItemTask } from "../controllers/TaskOperations";
+import {
+  addTask,
+  getTask,
+  deleteItemTask,
+  updateItemTask,
+} from "../controllers/TaskOperations";
 
 const route = Router();
 
 route.post("/newItem", addTask);
-route.get('/tasks',getTask)
-route.delete('/item/:id',deleteItemTask);
+route.get("/tasks", getTask);
+route.delete("/item/:id", deleteItemTask);
+route.put("/modify/:id", updateItemTask);
 export default route;
