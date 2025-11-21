0356fa565c54dcbc8c2cb12ff445f180cff64319
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2025-11-21 11:21:20 +0530

    Mock the service layer functions

--- src/tests/App.test.ts
+++ src/tests/App.test.ts
@@ -2,3 +2,10 @@ import request from 'supertest';
 import express from 'express';
 import route from '../routes/Routers';
 import { addTaskItem,getAllTasks,deleteTask,updateTask } from '../services/TaskService';
+
+jest.mock('../services/TaskService',()=>({
+    addTaskItem:jest.fn(),
+     getAllTasks: jest.fn(),
+    deleteTask:jest.fn(),
+    updateTask:jest.fn(),
+}));
